160bbacc16b87b2fd73cbb1f1279bf92dd0eed8a
annkura <annikura@yandex.ru>
2018-02-19 16:33:21 +0300

    Added an interface for FileSyncService

--- app/src/main/java/ru/spbau/lecturenotes/services/FileSyncService.java
+++ app/src/main/java/ru/spbau/lecturenotes/services/FileSyncService.java
@@ -1,6 +1,14 @@
 package ru.spbau.lecturenotes.services;
 
+import java.util.List;
+import java.util.function.Consumer;
+
+import ru.spbau.lecturenotes.storage.Attachment;
 import ru.spbau.lecturenotes.storage.DatabaseInterface;
+import ru.spbau.lecturenotes.storage.Document;
+import ru.spbau.lecturenotes.storage.ResultListener;
+import ru.spbau.lecturenotes.storage.identifiers.AttachmentId;
+import ru.spbau.lecturenotes.storage.identifiers.DocumentId;
 
 public class FileSyncService {
     protected DatabaseInterface db;
@@ -9,4 +17,28 @@ public class FileSyncService {
         this.db = db;
     }
 
+    protected void checkConsistency() {
+
+    }
+
+    public void cleanCache() {
+        // Пойти и удалить все файлы, которые не помечены сохраняемыми
+    }
+
+    public void onAttachmentsDownloaded(List<AttachmentId> attachments, ResultListener<List<Attachment>> listener) {
+        // Скачать аттачменты в кеш
+    }
+
+    public void addDocumentToCache() {
+        // Пометить документ сохраняемым
+        // Проверить, что он скачан
+        // Если нет, скачать.
+        // Пометить, что скачали.
+    }
+
+    public void onDocumentDownloaded(DocumentId documentId, ResultListener<Document> listener) {
+        // Пометить документ "скачивается"
+        // Поставить скачиваться
+        // Пометить скачанным
+    }
 }
