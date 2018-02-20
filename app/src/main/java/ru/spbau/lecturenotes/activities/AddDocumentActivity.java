0c1360686107ecef790ca3c930ec894b0668c048
annkura <annikura@yandex.ru>
2018-02-21 02:59:15 +0300

    Added activity for loading documents

---
+++ app/src/main/java/ru/spbau/lecturenotes/activities/AddDocumentActivity.java
@@ -0,0 +1,18 @@
+package ru.spbau.lecturenotes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import ru.spbau.lecturenotes.R;
+
+public class AddDocumentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_document);
+    }
+}
--- app/src/main/java/ru/spbau/lecturenotes/activities/DocumentListActivity.java
+++ app/src/main/java/ru/spbau/lecturenotes/activities/DocumentListActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -108,4 +110,31 @@ public class DocumentListActivity extends AppCompatActivity {
         documentsListener = null;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.document_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(),
+                    "Do you like settings?",
+                    Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id == R.id.action_add_document) {
+            Intent intent = new Intent(DocumentListActivity.this, AddDocumentActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
